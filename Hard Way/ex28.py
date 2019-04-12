b05cd31cbacc409d7f6edff2e9c20e4c7c912603
gl12138 <38601472+gl12138@users.noreply.github.com>
2019-04-12 10:28:15 +0800

    Chapter 06 09
    Chapter 06 09

---
+++ Hard Way/ex28.py
@@ -0,0 +1,11 @@
+if (True and True) and ("test" == "test") and (1 == 1 or 2 != 1) and (True and 1 == 1) and (True or 1 == 1) and ("test" != "testing") and (not (True and False)) and (not ("testing" == "testing" and "Zed" == "Cool Guy") ) and (1 == 1 and not ("testing" == 1 or 1 == 0)):
+	print(True)
+
+
+if (False and True) or (1 == 1 and 2 == 1) or (False and 0 != 0) or ("test" == "testing") or (1 != 0 and 2 == 1) or (not (1 == 1 and 0 != 1)) or ("test" == 1) or (not (10 == 1 or 1000 == 1000)) or (not (1 != 10 or 3 == 4)) or ("chunky" == "bacon" and not (3 == 4 or 3 == 3)) or (3 == 3 and not ("testing" == "testing" or "Python" == "Fun") ):
+	print(False)
+else:
+	print(True)
+
+
+print("1.  1;  2.  0;  3.  0;  4.  1 ;  5.  1;  6.  1;  7.  0;  8.  1;  9.  0;  10. 0;  11. 1;  12. 0;  13. 1;  14. 0;  15. 0;  16. 0;  17.1;  18. 1;  19. 0;  20. 0")
